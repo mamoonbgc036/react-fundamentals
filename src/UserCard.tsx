45e7845902f8fc96dd2f9f0e41cc316023dab221
mamoonbgc036 <mamoon036@gmail.com>
2026-03-03 21:46:45 +0600

    some change

--- src/UserCard.tsx
+++ src/UserCard.tsx
@@ -9,7 +9,7 @@ export default function UserCard(props: UserProps) {
     return (
         <div>
             <p>{props.name} - {props.email} - {props.role}</p>
-            {props.avatar ? (<img src={props.avatar} />) : (<p>No avatar provided</p>)}
+            {props.avatar ? <img src={props.avatar} /> : <p>No avatar provided</p>}
         </div>
     )
 }
