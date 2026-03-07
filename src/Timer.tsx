e9c8832f3bc38f07e9da946c7dc7308986c9a139
mamoonbgc036 <mamoon036@gmail.com>
2026-03-07 22:21:46 +0600

    timer update

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -22,6 +22,10 @@ export default function Timer() {
         return () => clearInterval(interval)
     }, [isRunning])
 
+    useEffect(() => {
+        document.title = "Its " + second
+    })
+
     return (
         <>
             <p>{second}</p>
