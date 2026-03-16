b1df700fd6e4803e6e947319305e49a6794523a7
mamoonbgc036 <mamoon036@gmail.com>
2026-03-16 06:18:38 +0600

    modify employeemanager

--- src/EmployeeManager.tsx
+++ src/EmployeeManager.tsx
@@ -14,9 +14,16 @@ export default function EmployeeManager() {
         ]
     )
 
+    const handleAdd = () => [
+        // setEmployee([...employees, {}])
+    ]
+
     return (
         <>
-            <button>Add</button>
+            <form action="">
+                <button onClick={handleAdd}>Add</button>
+            </form>
+
             <table><thead><tr><th>Name</th><th>Department</th><th>Salary</th><th>Action</th></tr></thead><tbody>
                 {
                     employees.map(employee => <tr><td>{employee.name}</td><td>{employee.department}</td><td>{employee.salary}</td><td><button>Delete</button></td></tr>)
