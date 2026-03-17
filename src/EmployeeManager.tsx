865e281fade5b6c5c106bdbd4280e26b38b979e7
mamoonbgc036 <mamoon036@gmail.com>
2026-03-17 18:00:03 +0600

    modify employee manager

--- src/EmployeeManager.tsx
+++ src/EmployeeManager.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 interface Employee {
+    id: number,
     name: string,
     department: string,
     salary: number
@@ -9,18 +10,25 @@ interface Employee {
 export default function EmployeeManager() {
     const [employees, setEmployee] = useState<Employee[]>(
         [
-            { name: 'mamoon', department: 'Finance', salary: 50000 },
-            { name: 'noman', department: 'Accounting', salary: 40000 }
+            { id: 1, name: 'mamoon', department: 'Finance', salary: 50000 },
+            { id: 2, name: 'noman', department: 'Accounting', salary: 40000 }
         ]
     )
 
     const [formData, setFormData] = useState<Employee>({
+        id: Date.now(),
         name: "",
         department: "",
         salary: 0
     })
 
-    const handleSubmit = () => {
+    const handleDelete = (id: number) => {
+        const newEmployees = employees.filter(employee => employee.id != id);
+        setEmployee(newEmployees)
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
         setEmployee([...employees, formData])
     }
 
@@ -38,9 +46,9 @@ export default function EmployeeManager() {
                 <button type="submit">Add</button>
             </form>
 
-            <table><thead><tr><th>Name</th><th>Department</th><th>Salary</th><th>Action</th></tr></thead><tbody>
+            <table><thead><tr><th>Id</th><th>Name</th><th>Department</th><th>Salary</th><th>Action</th></tr></thead><tbody>
                 {
-                    employees.map(employee => <tr><td>{employee.name}</td><td>{employee.department}</td><td>{employee.salary}</td><td><button>Delete</button></td></tr>)
+                    employees.map(employee => <tr key={employee.id}><td>{employee.id}</td><td>{Date.now()}</td><td>{employee.name}</td><td>{employee.department}</td><td>{employee.salary}</td><td><button onClick={() => handleDelete(employee.id)}>Delete</button></td></tr>)
                 }
             </tbody></table>
         </>
